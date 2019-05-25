bd423694ca5bd4807a2839022b761465cd3a7af0
wanbing <924177316@qq.com>
2019-05-25 18:13:47 +0800

    flutter plugin

--- lib/flutter_plugin_wan_toast.dart
+++ lib/flutter_plugin_wan_toast.dart
@@ -6,6 +6,7 @@ class FlutterPluginWanToast {
   static const MethodChannel _channel =
       const MethodChannel('flutter_plugin_wan_toast');
 
+
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
